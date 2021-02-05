9ccbe262353902e4111f06bf6767faa4caf4ae88
imprfct <kurchevskiy.personal@ya.ru>
2021-02-06 02:19:32 +0300

    Added better Camera control

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private void Update()
+    {
+        var pos = player.transform.position;
+        gameObject.transform.position = new Vector3(pos.x, pos.y + 20, pos.z - 20);
+    }
+}
