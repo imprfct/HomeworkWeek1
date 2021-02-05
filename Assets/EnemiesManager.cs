e976deb3f462916b14a76bafeabb4da6f5031a17
imprfct <kurchevskiy.personal@ya.ru>
2021-02-06 00:51:11 +0300

    Spawn enemies on fixedTime

--- Assets/EnemiesManager.cs
+++ Assets/EnemiesManager.cs
@@ -11,29 +11,39 @@ public class EnemiesManager : MonoBehaviour
     private GameObject enemyPrefab;
 
     private Vector3 _terrainDimensions;
+    private Vector3 _terrainPosition;
+    // Переменная помогает не спавнить врагов в горах :3
+    private float Borders = 30f;
     
     private void Awake()
     {
-        _terrainDimensions = GetComponent<Terrain>().terrainData.size / 2;
+        var terrain = GetComponent<Terrain>();
+        
+        _terrainDimensions = terrain.terrainData.size;
+        _terrainPosition = terrain.transform.position;
     }
 
     void SpawnEnemy()
     {
-        var minX = -_terrainDimensions.x;
-        var maxX = _terrainDimensions.x;
+        Debug.Log(_terrainPosition + " " + _terrainDimensions);
+        var minX = _terrainPosition.x + Borders;
+        var maxX = _terrainDimensions.x - Borders;
         
-        var minZ = -_terrainDimensions.z;
-        var maxZ = _terrainDimensions.z;
+        var minZ = _terrainPosition.z + Borders;
+        var maxZ = _terrainDimensions.z - Borders;
         
         var spawnPos = new Vector3(Random.Range(minX, maxX), 2, Random.Range(minZ, maxZ));
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity).GetComponent<enemyController>().target = player;
     }
 
-    private void Update()
+    private float newEnemyCooldown = 200f;
+    private void FixedUpdate()
     {
-        if (Input.GetKeyUp("w"))
+        newEnemyCooldown -= Time.fixedDeltaTime * 100;
+        if (newEnemyCooldown <= 0)
         {
             SpawnEnemy();
+            newEnemyCooldown = 200f;
         }
     }
 }
